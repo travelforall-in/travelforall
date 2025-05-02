46045442b54a8b8dc91c988780035eb4f72251e8
satyam330 <satyamjha9132@gmail.com>
2025-05-02 20:31:11 +0530

    updated Deployed Base-Url

--- client/src/utils/baseUrl.ts
+++ client/src/utils/baseUrl.ts
@@ -1,4 +1,10 @@
 const BASE_URL = 'http://localhost:5000/api'
+
+//    ----- updated BaseUrl line 3.
+// https://travelforall-backd.onrender.com/   
+
+
 //const BASE_URL = 'https://travelforall-7.onrender.com'
+
 export default BASE_URL;
 // https://travelforall-7.onrender.com
